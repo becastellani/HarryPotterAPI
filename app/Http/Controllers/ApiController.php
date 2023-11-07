fad8ce460f5f9c3650b92ede1283a398c38f0f16
becastellani <becastellani10@gmail.com>
2023-11-06 21:50:32 -0300

    cards

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,8 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class ApiController extends Controller
 {
-    //
+    public function getCharacters(Request $request)
+    {
+        // Defina o número de itens por página desejado, por exemplo, 10 itens por página.
+        $perPage = 9;
+
+        
+        $page = $request->query('page', 1);
+
+        $response = Http::get('https://hp-api.onrender.com/api/characters');
+        $characters = json_decode($response->body());
+
+       
+        $totalPages = ceil(count($characters) / $perPage);
+
+       
+        $paginatedCharacters = array_slice($characters, ($page - 1) * $perPage, $perPage);
+
+        return view('characters.index', compact('paginatedCharacters', 'page', 'totalPages'));
+    }
 }
---
+++ resources/views/characters/index.blade.php
@@ -0,0 +1,25 @@
+<head>
+    @vite('resources/css/app.css')
+</head>
+<div class="container mx-auto p-4">
+    <h1 class="text-3xl font-bold mb-4">Personagens de Harry Potter</h1>
+    <div class="grid grid-cols-3 gap-4">
+        @foreach ($paginatedCharacters as $character)
+            <div class="bg-white rounded-lg shadow-lg p-4">
+                <img src="{{ $character->image }}"  class="w-32 h-32 object-cover rounded-full mx-auto mb-2">
+                <p class="text-center font-bold">{{ $character->name }}</p>
+            </div>
+        @endforeach
+    </div>
+
+    <div class="mt-4 p-8 flex justify-between"> 
+        @if ($page > 1)
+            <a href="{{ route('characters', ['page' => $page - 1]) }}"  class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Página anterior</a>
+        @endif
+    
+        @if ($page < $totalPages)
+            <a href="{{ route('characters', ['page' => $page + 1]) }}"  class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Próxima página</a>
+        @endif
+    </div>
+    
+</div>
