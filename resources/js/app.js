6ebce68708269d1290ddd9d943b5bf50b46b96fe
Bernardo <becastellani10@gmail.com>
2023-11-08 17:37:53 -0300

    Modal

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,24 @@
 import './bootstrap';
+import '../css/app.css';
 
+
+document.querySelectorAll('.character-button').forEach(function(button) {
+    button.addEventListener('click', function() {
+        const target = button.getAttribute('data-target');
+        document.getElementById(target).style.display = 'block';
+    });
+});
+
+
+document.querySelectorAll('.close').forEach(function(closeButton) {
+    closeButton.addEventListener('click', function() {
+        const target = closeButton.getAttribute('id').replace('closeModal-', '');
+        document.getElementById(`modal-${target}`).style.display = 'none';
+    });
+});
+
+window.addEventListener('click', function(event) {
+    if (event.target.classList.contains('modal')) {
+        event.target.style.display = 'none';
+    }
+});
--- resources/views/characters/index.blade.php
+++ resources/views/characters/index.blade.php
@@ -1,29 +1,50 @@
 <head>
     @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
 </head>
 <div class="container mx-auto p-4">
     <h1 class="text-3xl font-bold mb-4">Personagens de Harry Potter</h1>
     <div class="grid grid-cols-3 gap-4">
         @foreach ($paginatedCharacters as $character)
             <div class="bg-white rounded-lg shadow-lg p-4">
-                <button class="mx-auto flex flex-col items-center">
-                    <img src="{{ $character->image }}"  class="w-32 h-32 object-cover rounded-full mx-auto mb-2">
+                <button class="mx-auto flex flex-col items-center character-button"
+                    data-target="modal-{{ $character->id }}">
+                    <img src="{{ $character->image }}" class="w-32 h-32 object-cover rounded-full mx-auto mb-2">
                     <p class="text-center font-bold">{{ $character->name }}</p>
-                    <p> {{ 'Data de Nascimento: ' . date('d/m/Y', strtotime($character->dateOfBirth)) }}</p>
-                    <p>{{'Patrono: ' . ucfirst($character->patronus ? $character->patronus : 'Não Identificado')}}</p>
+                    <p>Data de Nascimento: {{ date('d/m/Y', strtotime($character->dateOfBirth)) }}</p>
+                    <p>Patrono: {{ $character->patronus ? $character->patronus : 'Não Identificado' }}</p>
                 </button>
             </div>
         @endforeach
+
+        {{-- foreach do modal aqui --}}
+        @foreach ($paginatedCharacters as $character)
+            <div id="modal-{{ $character->id }}" class="modal">
+                <div class="modal-content">
+                    <span class="close" id="closeModal-{{ $character->id }}">&times;</span>
+                    <h2>Detalhes do Personagem</h2>
+                    <div id="modalContent-{{ $character->id }}">
+                        <div class="mx-auto flex flex-col items-center">
+                            <img src="{{ $character->image }}" class="w-32 h-32 object-cover rounded-full mx-auto mb-2">
+                            <h2 class="font-semibold text-lg">{{ $character->name }}</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+</div>
+
+
+    <div class="mt-4 p-8 flex justify-between">
+        @if ($page > 1)
+        <a href="{{ route('characters', ['page' => $page - 1]) }}"
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Página anterior</a>
+        @endif
+
+        @if ($page < $totalPages) <a href="{{ route('characters', ['page' => $page + 1]) }}"
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Próxima página</a>
+            @endif
     </div>
 
-    <div class="mt-4 p-8 flex justify-between"> 
-        @if ($page > 1)
-            <a href="{{ route('characters', ['page' => $page - 1]) }}"  class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Página anterior</a>
-        @endif
-    
-        @if ($page < $totalPages)
-            <a href="{{ route('characters', ['page' => $page + 1]) }}"  class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Próxima página</a>
-        @endif
-    </div>
-    
 </div>
